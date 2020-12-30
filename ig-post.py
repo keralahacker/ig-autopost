e2139007fa2181fd09f36ae40c0ad61c5b770157
Hackwithvyshu <64751167+keralahacker@users.noreply.github.com>
2020-12-30 21:42:28 +0530

    Create ig-post.py

---
+++ ig-post.py
@@ -0,0 +1,18 @@
+from instabot import Bot 
+  
+  
+bot = Bot() 
+  
+bot.login(username = "user_name",  
+          password = "user_password") 
+  
+# Recommended to put the photo 
+# you want to upload in the same 
+# directory where this Python code 
+# is located else you will have  
+# to provide full path for the photo 
+bot.upload_photo("Hack_with_vyshu.jpg", 
+                 caption ="Automatio script
+                 for photo upload
+
+       #Instagram.com/hack_with_vyshu
